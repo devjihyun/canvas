9b55b707878275d9058641c2a622f32c33eba0fd
sonia <sonia1.kim@cheilpengtai.com>
2022-12-02 18:36:36 +0900

    지우기, 리셋 추가

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const modeBtn = document.getElementById("mode-btn");
+const resetBtn = document.getElementById("reset-btn");
+const eraserBtn = document.getElementById("eraser-btn");
 const colorOptions = Array.from(
     document.getElementsByClassName("color-option")
 );
@@ -7,8 +9,11 @@ const linewidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d"); // context = brush 역할
 
-canvas.width = 800;
-canvas.height = 800;
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 800;
+
+canvas.width =  CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
 ctx.lineWidth = linewidth.value;
 
 let isPainting = false;
@@ -51,17 +56,28 @@ function onColorClick(event) {
 function onModeClick() {
     if(isFilling) {
         isFilling = false
-        modeBtn.innerText = "Fill Mode"
+        modeBtn.innerText = "Fill"
     } else {
         isFilling = true
-        modeBtn.innerText = "Draw Mode"
+        modeBtn.innerText = "Draw"
     }
 }
 
 function onCanvasClick() {
     if(isFilling) {
-        ctx.fillRect(0, 0, 800, 800);
+        ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     }
+}
+
+function onResetClick() {
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+}
+
+function onEraserClick() {
+    ctx.strokeStyle = "white";
+    isFilling = false;
+    modeBtn.innerText = "Fill"
 }
 
 canvas.addEventListener("mousemove", onMove);
@@ -77,3 +93,5 @@ colorOptions.forEach((color) => color.addEventListener("click",
 onColorClick));
 
 modeBtn.addEventListener("click", onModeClick);
+resetBtn.addEventListener("click", onResetClick);
+eraserBtn.addEventListener("click", onEraserClick);
